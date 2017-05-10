65cffb9910342a79110669122f317e2bd8902f29
xiyeweiliang <pangjinpo@163.com>
2017-05-10 16:27:30 +0800

    add my solution 476. Number Complement
    Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
    
    Note:
    The given integer is guaranteed to fit within the range of a 32-bit signed integer.
    You could assume no leading zero bit in the integer’s binary representation.
    Example 1:
    Input: 5
    Output: 2
    Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
    Example 2:
    Input: 1
    Output: 0
    Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.

---
+++ findComplement.c
@@ -0,0 +1,16 @@
+int findComplement(int num) {
+    int tmp = num;
+    int ret = 0;
+    int index = 0;
+    
+    while (tmp && (index < 32))
+    {
+        if (!(tmp & 0x1))
+            ret |= (0x1 << index);
+        
+        tmp >>= 1;    
+        index++;
+    }
+    
+    return ret;
+}
