c1d6b5536ac535fa227b82a4183fff7d45cedf2f
xiyeweiliang <pangjinpo@163.com>
2017-05-18 10:33:22 +0800

    Update islandPerimeter.c

--- islandPerimeter.c
+++ islandPerimeter.c
@@ -1,3 +1,4 @@
+/* my solution 119ms */
 int islandPerimeter(int** grid, int gridRowSize, int gridColSize) {
     int row, col;
     int perimeter = 0;
@@ -28,4 +29,36 @@ int islandPerimeter(int** grid, int gridRowSize, int gridColSize) {
     }
     
     return perimeter;
+}
+
+
+/* another solution 100ms */
+int islandPerimeter(int** grid, int gridRowSize, int gridColSize) {
+   int grith = 0;
+   for(int i = 0; i < gridRowSize; i++)
+   {
+      for (int j = 0; j < gridColSize; j++)
+      {
+         if(grid[i][j] == 1)
+         {
+            if (((i - 1) < 0 ) || (grid[i-1][j] == 0))
+            {
+               grith++;
+            }
+            if (((j - 1) < 0 ) || (grid[i][j-1] == 0))
+            {
+               grith++;
+            }
+            if (((i + 1) >= gridRowSize ) || (grid[i+1][j] == 0))
+            {
+               grith++;
+            }
+            if (((j + 1) >= gridColSize ) || (grid[i][j+1] == 0))
+            {
+               grith++;
+            }
+         }   
+      }
+   }
+   return grith; 
 }
