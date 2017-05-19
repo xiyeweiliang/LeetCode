c0759b7f7bbd37f3b7fa1bdfbcc607b007f01dbb
xiyeweiliang <pangjinpo@163.com>
2017-05-19 17:06:24 +0800

    Update findMaxConsecutiveOnes.c

--- findMaxConsecutiveOnes.c
+++ findMaxConsecutiveOnes.c
@@ -1,3 +1,4 @@
+/* my splution */
 int findMaxConsecutiveOnes(int* nums, int numsSize) {
     int loop = 0;
     int max = 0;
@@ -19,4 +20,30 @@ int findMaxConsecutiveOnes(int* nums, int numsSize) {
     }
         
     return max;            
+}
+
+/* another solution 1*/
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+ int max = 0;
+ int sum = 0;
+ for (int i=0; i<numsSize; i++)
+ {
+     sum = (sum+nums[i])*nums[i];
+     if(max<sum){max=sum;}
+ }
+return max;
+}
+
+/* another solution 2*/
+int findMaxConsecutiveOnes(int* nums, int numsSize)
+{
+    int max = 0, i, count = 0;
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] == 1)
+            count++;
+        else
+            count = 0;
+        max = max < count ? count : max;
+    }
+    return max;
 }
