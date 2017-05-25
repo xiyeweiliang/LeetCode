43aa0966b7face170cc055a4e5c7a7f53e5bf73a
xiyeweiliang <pangjinpo@163.com>
2017-05-25 14:19:02 +0800

    Update moveZeroes.c

--- moveZeroes.c
+++ moveZeroes.c
@@ -1,3 +1,4 @@
+/* my solution */
 void moveZeroes(int* nums, int numsSize) {
     int value = 0;
     int zero = 0;
@@ -21,4 +22,23 @@ void moveZeroes(int* nums, int numsSize) {
     }
     
     return;
+}
+
+/* another solution */
+void moveZeroes(int* nums, int numsSize) {
+    int lastNonZeroFoundAt = 0;
+    int i;
+    // If the current element is not 0, then we need to
+    // append it just in front of last non 0 element we found. 
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroFoundAt++] = nums[i];
+        }
+    }
+    // After we have finished processing new elements,
+    // all the non-zero elements are already at beginning of array.
+    // We just need to fill remaining array with 0's.
+    for (i = lastNonZeroFoundAt; i < numsSize; i++) {
+        nums[i] = 0;
+    }
 }
